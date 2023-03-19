555a6ae4b382568d5b35d971a9006ca7a6c280ca
vgpur <vgpur@LAPTOP-HR52MGN3>
2023-03-19 20:27:05 -0300

    Add project files.

---
+++ ATIVIDADE03AED/Program.cs
@@ -0,0 +1,21 @@
+﻿using ATIVIDADE03AED;
+using System;
+using System.Globalization;
+namespace atividade03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vendedor v = new Vendedor();
+
+            Console.WriteLine("Digite o nome do vendedor: ");
+            v.Nome = Console.ReadLine();
+            Console.WriteLine("Digite o salário fixo do vendedor: ");
+            v.SalarioFixo = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o total em vendas: ");
+            v.TotalVendas = double.Parse(Console.ReadLine());
+            v.imprimirDados();
+        }
+    }
+}
---
+++ ATIVIDADE03AED/Vendedor.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATIVIDADE03AED
+{
+    internal class Vendedor
+    {
+        private string nome;
+        private double salarioFixo;
+        private double totalVendas;
+        private double valorComissao = 0.15;
+
+
+
+        public string Nome
+        {
+            get
+            {
+                return nome;
+            }
+
+            set
+            {
+                nome = value;
+            }
+        }
+        public double SalarioFixo
+        {
+            get 
+            { 
+            return salarioFixo; 
+            }
+            set
+            {
+                salarioFixo = value;
+            }
+        }
+        public double TotalVendas 
+        {
+            get
+            {
+                return totalVendas;
+            }
+            set
+            {
+                totalVendas = value;
+            }
+        }
+        public void imprimirDados()
+        {
+            double salarioFinal = salarioFixo + (totalVendas * valorComissao);
+            Console.WriteLine($"{nome} recebeu de salário + comissão R$ {salarioFinal:0.00}");
+        }
+    }
+
+}
